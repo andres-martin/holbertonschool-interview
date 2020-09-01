359573a87f8319dd71e1c2066471e71c506fef9a
andres-martin <andresmartin266@gmail.com>
2020-08-31 20:37:56 -0500

    [add] status codes

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""  script that reads stdin line by line and computes metrics """
+
+import sys
+
+STATUS_CODES = {'200': 0,
+                '301': 0, '400': 0,
+                '401': 0, '403': 0,
+                '404': 0, '405': 0,
+                '500': 0}
+
+total_size = 0
+number_of_lines = 0
+
+
+def print_status_code(total_size):
+    print("File size:", total_size)
+    for key, value in STATUS_CODES.items():
+        if value != 0:
+            print("{}: {:d}".format(key, value))
+
+
+try:
+    for argument in sys.stdin:
+        arguments = argument.split(" ")
+        status_code = arguments[-2]
+        file_size = arguments[-1]
+
+        STATUS_CODES[status_code] += 1
+
+        total_size += int(file_size)
+        number_of_lines += 1
+
+        if number_of_lines == 10:
+            number_of_lines = 0
+            print_status_code(total_size)
+
+except KeyboardInterrupt:
+    pass
+finally:
+    print_status_code(total_size)
