14b0953023a728c02a4bb089c3861a04dcd38050
andres-martin <andresmartin266@gmail.com>
2020-11-24 22:13:32 -0500

    [add] rain algorithm

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''rain algo'''
+from typing import List
+
+
+def rain(walls: List[int]) -> int:
+    ''' Return: Integer indicating
+        total amount of rainwater retained.
+    '''
+    if not walls or len(walls) == 0:
+        return 0
+
+    return sum(walls[1:-2])
